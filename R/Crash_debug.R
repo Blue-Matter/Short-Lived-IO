c538da7149e646934147585de6a3f04e1a36d3bb
tcarruth <tomsinbox@gmail.com>
2026-05-27 10:45:52 -0700

    additional tests

---
+++ R/Crash_debug.R
@@ -0,0 +1,7 @@
+library(MSEtool)
+
+om1 = readRDS("C:/temp/OM1.rds")
+hist = Simulate(om1)
+
+om2 = readRDS("C:/temp/OM2.rds")
+hist = Simulate(om2)
--- R/Latest tests May 2026.R
+++ R/Latest tests May 2026.R
@@ -2,6 +2,9 @@
 # ===== Demonstration of Sim - Sam ===================================================
 # ====================================================================================
 
+# remotes::install_github("DTUAqua/spict/spict")
+# remotes::install_github("Blue-Matter/slMSE")
+
 
 library(slMSE)
 packageVersion('slMSE')
@@ -35,8 +38,8 @@ CurrentYear = 2026
 source("C:/GitHub/Short-Lived-IO/R/Custom_stocks.R") # Three short-lived morphs
 source("C:/GitHub/Short-Lived-IO/R/Custom_fleets.R") # Two fleets (identical fishing dynamics among morphs)
 
-om = slOM(stock = stock, fleet = fleet, ComplexName = "Pacific JFS", 
-          nSim = nSim, nYear = nYear, pYear = pYear, Seasons = Seasons, 
+om = slOM(stock = stock, fleet = fleet, ComplexName = "Pacific JFS",
+          nSim = nSim, nYear = nYear, pYear = pYear, Seasons = Seasons,
           CurrentYear = CurrentYear, Interval = 6, Seed = 1)
 
 hist = Simulate(om)
@@ -46,8 +49,24 @@ slplot(hist)
 # --- Sim Testing Stock Assessment Methods ------------------------------
 
 simdata = slSimData(hist)
+slplot(simdata)
 
 
+# --- SPiCT ----------
+
+library(spict)
+sim = 1
+Sdata = SPiCT_data(sim, simdata)
+Sinput = SPiCT_config(Sdata)
+fit = fit.spict(Sinput)
+
+# --- JABBA ----------
+
+
+# --- RCM - ASPM -----
+
+
+# --- RCM - SCAL -----
 
 
 
