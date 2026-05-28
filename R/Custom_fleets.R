2a1d68ba5c09bb1bac41b58516da38fb40737787
tcarruth <tomsinbox@gmail.com>
2026-05-28 14:43:01 -0700

    SPiCT sim sam 3/3

--- R/Custom_fleets.R
+++ R/Custom_fleets.R
@@ -1,8 +1,7 @@
+Short_lowF = slFleet(Name = "Short_lowF", sel50 = 1, selSLP = 4, relE_min = 0.3, relE_max = 0.75, maxF = 1.3, nYear = nYear, pYear = pYear, Seasons = Seasons, nSim = nSim, nAges = nAges, plot=T)
+Long_highF = slFleet(Name = "Long_highF", sel50 = 3.0, selSLP = 5, relE_loc = 0.1, relE_freq = 1, maxF = 0.65, smu = 0.7, scv = 0.15, nYear = nYear, pYear = pYear, Seasons = Seasons, nSim = nSim, nAges = nAges, plot=T)
 
-Long_highF = slFleet(Name = "Long_highF", sel50 = 3.0, selSLP = 5, relE_loc = 0.1, relE_freq = 1, maxF = 0.2, smu = 0.7, scv = 0.15, nYear = nYear, pYear = pYear, Seasons = Seasons, nSim = nSim, nAges = nAges, plot=T)
-Short_lowF = slFleet(Name = "Short_lowF", sel50 = 1, selSLP = 4, relE_min = 0.5, relE_max = 0.75, maxF = 0.1, nYear = nYear, pYear = pYear, Seasons = Seasons, nSim = nSim, nAges = nAges, plot=T)
-
-fleet = slCombineFleets(fleetlist = list(Long_highF = Long_highF, Short_lowF = Short_lowF), stock)
+fleet = slCombineFleets(fleetlist = list( Short_lowF = Short_lowF, Long_highF = Long_highF), stock)
 
 cat("Returing a StockFleetList object 'fleet' containing two identical fleets \n")
 
--- R/Latest tests May 2026.R
+++ R/Latest tests May 2026.R
@@ -18,21 +18,21 @@ packageVersion('MSEtool')
 
 # === Single substock ==========================================================
 
-om = slOM()
-hist = Simulate(om)
-slplot(hist)
+# om = slOM()
+# hist = Simulate(om)
+# slplot(hist)
 
 
 # === Multi stock ==============================================================
 
 # --- Historical Simulation --------------------------------------------
 
-nYear = 15  # No. historical years
+nYear = 25  # No. historical years
 pYear = 15  # No. projection years
 Seasons = 4 # time steps, subyears/seasons per year (two-monthly)
 nAreas = 3
 nAges = 8
-nSim = 4
+nSim = 24
 CurrentYear = 2026
 
 source("C:/GitHub/Short-Lived-IO/R/Custom_stocks.R") # Three short-lived morphs
@@ -42,7 +42,7 @@ om = slOM(stock = stock, fleet = fleet, ComplexName = "Pacific JFS",
           nSim = nSim, nYear = nYear, pYear = pYear, Seasons = Seasons,
           CurrentYear = CurrentYear, Interval = 6, Seed = 1)
 
-hist = Simulate(om)
+hist = Simulate(om, doMSYRefs=T)
 slplot(hist)
 
 
@@ -55,34 +55,22 @@ slplot(simdata)
 # --- SPiCT ----------
 
 library(spict)
-sim = 1
+Sout = do_spict(sim = 1, simdata)
 
-do_spict(1,simdata)
-
-#  r.pr = c(0.5,0.2,1); bk.pr = c(0.5,0.3,1);shape.pr = c(2, 0.001, 1);oe = c(0.2, 0.5, 1);pe = c(0.2,0.5,1);fdevs = c(4, 0.5, 1);ce = c(0.05, 0.001, 1);q.pr = NULL;timing = 0.625;dteuler = 0.25
-
-do_spict=function(sim, simdata,
-                  r.pr = c(0.5,0.2,1),
-                  bk.pr = c(0.5,0.3,1),
-                  shape.pr = c(2, 0.001, 1),
-                  oe = c(0.2, 0.5, 1),
-                  pe = c(0.2,0.5,1),
-                  fdevs = c(4, 0.5, 1),
-                  ce = c(0.05, 0.001, 1),
-                  q.pr = NULL,
-                  timing = 0.625,
-                  dteuler = 0.25){
-
-  Sdata = SPiCT_data(sim, simdata)
-  Sinput = SPiCT_config(Sdata, r.pr, bk.pr, shape.pr, oe, pe, fdevs, ce,  q.pr, timing, dteuler)
-  fit = fit.spict(Sinput)
-  SPiCT_output(fit)
-
-}
-
-cbind(simdata$B[sim, ],Sout$B, Sout$B/simdata$B[sim, ])
+SS_spict = SimSam_spict(simdata, parallel =T,
+              r.pr = c(0.5,0.2,1),
+              bk.pr = c(0.5,0.3,1),
+              shape.pr = c(2, 0.001, 1),
+              oe = c(0.2, 0.5, 1),
+              pe = c(0.2, 0.5, 1),
+              fdevs = c(4, 0.5, 1),
+              ce = c(0.05, 0.001, 1),
+              q.pr = NULL,
+              timing = 0.625,
+              dteuler = 0.25)
 
 
+slplot(SS_spict)
 
 # --- JABBA ----------
 
@@ -98,7 +86,11 @@ cbind(simdata$B[sim, ],Sout$B, Sout$B/simdata$B[sim, ])
 # ====== END =========================================================================
 # ====================================================================================
 
-
+myfunc = function(...){
+  dots = list(...)
+  cat(paste0(names(dots),dots))
+  dots
+}
 
 
 
