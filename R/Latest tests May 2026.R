a34b33f58eb7d07af4d14a2d2f0c7f4bfe992a39
tcarruth <tomsinbox@gmail.com>
2026-05-28 14:50:27 -0700

    Update Latest tests May 2026.R

--- R/Latest tests May 2026.R
+++ R/Latest tests May 2026.R
@@ -58,18 +58,19 @@ library(spict)
 Sout = do_spict(sim = 1, simdata)
 
 SS_spict = SimSam_spict(simdata, parallel =T,
-              r.pr = c(0.5,0.2,1),
+              r.pr = c(0.8,0.2,1),
               bk.pr = c(0.5,0.3,1),
               shape.pr = c(2, 0.001, 1),
               oe = c(0.2, 0.5, 1),
-              pe = c(0.2, 0.5, 1),
+              pe = c(0.5, 0.5, 1),
               fdevs = c(4, 0.5, 1),
               ce = c(0.05, 0.001, 1),
               q.pr = NULL,
-              timing = 0.625,
-              dteuler = 0.25)
+              timing = 0.1,
+              dteuler = 0.1)
 
 
+# slsumm(SS_spict)
 slplot(SS_spict)
 
 # --- JABBA ----------
